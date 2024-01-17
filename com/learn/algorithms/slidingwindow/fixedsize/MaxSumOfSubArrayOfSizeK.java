ba930806925e9f97015c3b37c1bf7887c44a747c
mayankksinha1 <mayankksinha1@gmail.com>
2024-01-17 22:30:43 +0530

    Maximum of sub array of size K

---
+++ com/learn/algorithms/slidingwindow/fixedsize/MaxSumOfSubArrayOfSizeK.java
@@ -0,0 +1,7 @@
+package com.learn.algorithms.slidingwindow.fixedsize;
+
+public class MaxSumOfSubArrayOfSizeK {
+    public static void main(String[] args) {
+        System.out.println("Sliding Window");
+    }
+}
