8512315b638bd1523264f46c74efd0a8a880fb46
mayankksinha1 <mayankksinha1@gmail.com>
2024-12-03 16:02:02 +0530

    Added codes to some problems

--- com/learn/algorithms/contests/ArrayStateafterKOperationsII.java
+++ com/learn/algorithms/contests/ArrayStateafterKOperationsII.java
@@ -1,5 +1,7 @@
 package com.learn.algorithms.contests;
 
+import java.util.PriorityQueue;
+
 public class ArrayStateafterKOperationsII {
     public static void main(String[] args) {
        int[] nums = {2,1,3,5,6};
@@ -12,15 +14,17 @@ public class ArrayStateafterKOperationsII {
 
     public static int[] getFinalState(int[] nums, int k, int multiplier) {
         int m = 1000000007;
-        for(int i = 0; i < k; i++) {
-            int index = 0;
-            for(int j=1; j < nums.length; j++) {
-                if(nums[index] > nums[j]) {
-                    index = j;
-                }
-            }
-            long current = (long) nums[index] * multiplier;
-            nums[index] = (int) (current%m);
+        PriorityQueue<int[]> queue = new PriorityQueue<>((a, b) -> a[0]!=b[0] ? a[0] - b[0] : a[1] - b[1]);
+
+        for(int i = 0; i < nums.length; i++) {
+            queue.offer(new int[]{nums[i], i});
+        }
+        while(k > 0) {
+            int[] min = queue.poll();
+            long current = (long) min[0] * multiplier;
+            nums[min[1]] = (int) (current%m);
+            queue.offer(new int[]{(int) (current%m), min[1]});
+            k--;
         }
         return nums;
     }
